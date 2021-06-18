4283a74c33cd8731c92d2040aef2cbc517612c12
Victor Naumov <kerlon5@mail.ru>
2021-06-18 21:39:44 +0500

    implement firebase utils

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,7 +45,7 @@ class SignIn extends Component {
                         required />
                     <div className="buttons">
                         <CustomButton type="submit" value="Submit Form">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             {' '}Sign in with Google{' '}
                         </CustomButton>
                     </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ var firebaseConfig = {
     measurementId: "G-M9L267WRWF"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    if (!snapShot.exists) {
+      const {displayName, email} = userAuth
+      const createdAt = new Date()
+      try{
+        await userRef.set({
+          displayName, 
+          email,
+          createdAt,
+          ...additionalData
+        })
+      }
+      catch (error){
+        console.log(error.message)
+      }
+    }
+    console.log(snapShot)   
+    return userRef
+  }
+
   firebase.initializeApp(firebaseConfig)
 
   export const auth = firebase.auth()
